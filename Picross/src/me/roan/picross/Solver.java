74875e3f6ca118e6cc3145305c87839dffaf7728
Roan Hofland <roan.hofland@hotmail.com>
2020-09-12 16:24:23 +0200

    Solver start

---
+++ Picross/src/me/roan/picross/Solver.java
@@ -0,0 +1,25 @@
+package me.roan.picross;
+
+public class Solver{
+	private Board board;
+	
+	public Solver(Board board){
+		this.board = board;
+	}
+	
+	public void solveAll(){
+		
+	}
+	
+	public void solveOne(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
